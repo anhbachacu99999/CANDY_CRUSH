b60116b6dd46a23c887d7ae9e0346f24d81cfa61
anhbachacu99999 <tranhuyhoang23032007@gmail.com>
2024-10-13 12:51:10 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,18 @@
 var Keo=["Red", "Yellow", "Green", "Blue", "Orange", "Purple"];
 var row, col, score=0;
-window.onload = function()
-{
-    startGame();
-    window.setInterval(function()
-    {
-        crushCandy();
-        slideCandy();
-        generateCandy();
-    }, 100);
-}
-function randomCandy() 
-{
-    return candies[Math.floor(Math.random() * candies.length)]; //0 - 5.99
-}
 row=Number(document.getElementById("rows").value);
 col=Number(document.getElementById("colums").value);
-
+window.onload = function() //hàm được gọi khi mở trang web
+{
+    startGame(); //chứa các khỏi tạo cho game
+    window.setInterval(function()
+    {
+        crushCandy(); //phá kẹo
+        slideCandy(); //di chuyển kẹo
+        generateCandy(); //tạo ra kẹo
+    }, 100);
+}
+function randomKeo() //chọn kẹo ngẫu nhiên
+{
+    return Keo[Math.floor(Math.random() * Keo.length)]; 
+}
