7bbb2f4d4c3628d09ab44ab4854fa0041b5cbc7c
anhbachacu99999 <tranhuyhoang23032007@gmail.com>
2024-10-13 12:37:51 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
+var Keo=["Red", "Yellow", "Green", "Blue", "Orange", "Purple"];
+var row, col, score=0;
+window.onload = function()
+{
+    startGame();
+    window.setInterval(function()
+    {
+        crushCandy();
+        slideCandy();
+        generateCandy();
+    }, 100);
+}
+function randomCandy() 
+{
+    return candies[Math.floor(Math.random() * candies.length)]; //0 - 5.99
+}
+row=Number(document.getElementById("rows").value);
+col=Number(document.getElementById("colums").value);
 
