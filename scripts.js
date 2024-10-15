96fbb5c22107450b81c053991b83fd81619bbadb
anhbachacu99999 <tranhuyhoang23032007@gmail.com>
2024-10-16 06:12:28 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,43 @@ var Keo=["Red", "Yellow", "Green", "Blue", "Orange", "Purple"];
 var row, col, score=0;
 row=Number(document.getElementById("rows").value);
 col=Number(document.getElementById("colums").value);
+function Tao_Bang()
+{
+    //var inputSection=document.getElementById("game");
+    //inputSection.style.display="none";
+    document.getElementById("diem").innerHTML="Score:"
+    // Tạo bảng
+    const newSquare=document.createElement("div");
+    newSquare.classList.add("bang");
+    // Thêm bảng vào trang
+    const squareContainer=document.getElementById("game");
+    squareContainer.appendChild(newSquare);
+    // Ẩn hộp input
+    //const inputSection=document.getElementById("Input_");
+    //inputSection.style.display="none";
+}
+/*function randomKeo() //sinh giá trị ngẫu nhiên trong mảng Keo[]
+{
+    return Keo[Math.floor(Math.random() * Keo.length)]; //0 - 5.99
+}
+function start()
+{
+    for(let h=0; h<row; h++)
+    {
+        let hang=[];
+        for(let c=0; c<col; c++)
+        {
+            let phan_tu=document.createElement("img");
+            phan_tu.id=h.toString()+"-"+c.toString();
+            phan_tu.src="C:/TranHuyHoang/Candy"+randomKeo()+".png";
+        }
+    }
+}
 function START()
 {
     window.onload = function() 
     {
-        startGame(); //chứa các khỏi tạo cho game
+        start(); //chứa các khỏi tạo cho game
         window.setInterval(function () 
         {
             Pha_Keo(); //phá kẹo
@@ -14,9 +46,4 @@ function START()
             Tao_Keo(); //tạo ra kẹo
         }, 100);
     }
-    function randomKeo() //sinh giá trị ngẫu nhiên trong mảng Keo[]
-    {
-        return Keo[Math.floor(Math.random() * Keo.length)]; //0 - 5.99
-    }
-    
-}
+}*/
